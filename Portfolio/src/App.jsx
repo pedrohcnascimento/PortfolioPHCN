a79b084f48d28b987f279d9094fc808ce4aa6a83
PHCN <pedrohenrique.cn07@gmail.com>
2026-03-03 20:03:36 -0300

    Primeiro commit, criando o portfólio

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return(
+    <div className="App">
+      <h1>My Portfolio</h1>
+      <p>Welcome to my portfolio website! Here you can find information about my projects, skills, and experience.</p>
+      <h2>Projects</h2>
+      <ul>
+        <li><a href="https://github.com/pedrohcnascimento/ProjectAurora">Project Aurora</a> - Exercises for ai training databases.</li>
+        <li><a href="https://github.com/Arthur2060/Inpark-backend">Inpark Backend</a> - Backend for a parking management system.</li>
+        <li><a href="https://github.com/DiegoGenuino/frontend-inpark">Inpark Frontend</a> - Frontend for a parking management system.</li>
+      </ul>
+
+
+    </div>
+  )
+}
+
+export default App
