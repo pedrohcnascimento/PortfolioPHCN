7696d2810b5965204b4a1425d1b0c1c45619bd9a
PHCN <pedrohenrique.cn07@gmail.com>
2026-03-04 22:10:34 -0300

    Adicionando o speedInsights

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Dashboard from './pages/dashboard/Dashboard'
 import Topbar from './components/topbar/Topbar'
 
